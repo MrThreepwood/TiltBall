fea6d334fe1b11f8ed5f638de70a35f231d7a220
teebird <tee.bird@comcast.net>
2014-06-15 16:37:15 -0500

    darkscreen and reset implemented

--- Tilt Ball/src/main/java/com/github/mrthreepwood/tiltball/tiltball/BallView.java
+++ Tilt Ball/src/main/java/com/github/mrthreepwood/tiltball/tiltball/BallView.java
@@ -1,31 +1,176 @@
 package com.github.mrthreepwood.tiltball.tiltball;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.PointF;
+import android.graphics.RectF;
+import android.os.Handler;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class BallView extends View {
 
     public float x;
     public float y;
     private final int r;
-    private final Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Handler mHandler = new Handler(); //so redraw occurs in main thread
+    Timer mTmr = null;
+    public int mScrWidth, mScrHeight;
+    float dragCoeff = .02f;
+    View view;
+    PointF mBallSpd = new PointF(0,0);
+    PointF mBallAcl = new PointF(0,0);
+    PointF coin1Pos = new PointF(0,0);
+    PointF coin2Pos = new PointF(0,0);
+    float coinSize = 8;
+    int coinsColected = 0;
+    long time;
+    Context context;
+    boolean coin1 = true;
+    boolean coin2 = true;
+
+
+    private final Paint ballColor = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private final Paint coinColor = new Paint(Paint.ANTI_ALIAS_FLAG);
 
     //construct new ball object
-    public BallView(Context context, float x, float y, int r) {
+    public BallView(Context context) {
         super(context);
+        this.context = context;
         //color hex is [transparency][red][green][blue]
-        mPaint.setColor(0xFF00FF00);  //not transparent. color is green
-        this.x = x;
-        this.y = y;
-        this.r = r;  //radius
+        ballColor.setColor(0xFF00FF00);  //not transparent. color is green
+        coinColor.setColor(0xffffff00);
+        WindowManager window = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = window.getDefaultDisplay();
+        mScrWidth = display.getWidth();
+        mScrHeight = display.getHeight();
+        x = mScrWidth / 2;
+        y = mScrHeight / 2;
+        r = 12;  //radius
+        debugWindow(context);
+        placeCoin(1);
+        placeCoin(2);
+        time = System.currentTimeMillis()/1000;
+        //create timer to move ball to new position
+    }
+    public void startTimer() {
+        mTmr = new Timer();
+        mTmr.schedule(new MyTimerTask(), 10, 10);
     }
 
     //called by invalidate()
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawCircle(x, y, r, mPaint);
+        canvas.drawColor(0xff000000);
+        if (coin1) {
+            double xDiff1 = x - coin1Pos.x;
+            double yDiff1 = y - coin1Pos.y;
+            if (xDiff1 * xDiff1 + yDiff1 * yDiff1 < 400) {
+                placeCoin(1);
+            }
+            if(coin1)
+                canvas.drawCircle(coin1Pos.x, coin1Pos.y, coinSize, coinColor);
+        }
+        if (coin2) {
+            double xDiff2 = x-coin2Pos.x;
+            double yDiff2 = y-coin2Pos.y;
+            if (xDiff2 * xDiff2 + yDiff2 * yDiff2 < 400) {
+                placeCoin(2);
+            }
+            if (coin2)
+                canvas.drawCircle(coin2Pos.x, coin2Pos.y, coinSize, coinColor);
+        }
+        canvas.drawCircle(x, y, r, ballColor);
+    }
+    public void placeCoin (int coin) {
+        if (coinsColected >= 10) {
+            if (coin == 1)
+                coin1 = false;
+            if (coin == 2)
+                coin2 = false;
+        }
+        if (coin == 1) {
+            coin1Pos.x = (float) Math.random()*mScrWidth;
+            coin1Pos.y = (float) Math.random()*mScrHeight;
+        }
+        if (coin == 2) {
+            coin2Pos.x = (float) Math.random()*mScrWidth;
+            coin2Pos.y = (float) Math.random()*mScrHeight;
+        }
+        coinsColected ++;
+        if (coinsColected >= 12) {
+            long completedIn = System.currentTimeMillis()/1000- time;
+            CharSequence text = "You collected 10 coins in " + completedIn + " seconds!";
+            Toast congrats = Toast.makeText(context, text, Toast.LENGTH_LONG);
+            congrats.show();
+        }
+    }
+    class MyTimerTask extends TimerTask {
+        public void run() {
+            //if debugging with external device,
+            //  a log cat viewer will be needed on the device
+            //android.util.Log.d("TiltBall", "Timer Hit - " + mBallPos.x + ":" + mBallPos.y);
+            //move ball based on current speed
+            if ((mBallAcl.x < .02f && mBallAcl.x > 0) || (mBallAcl.x > -.02f && mBallAcl.x < 0))
+                mBallAcl.x =0;
+            if ((mBallAcl.y < .02f && mBallAcl.y > 0) || (mBallAcl.y > -.02f && mBallAcl.y < 0))
+                mBallAcl.y =0;
+            mBallAcl.x -= dragCoeff*mBallSpd.x;
+            mBallAcl.y -= dragCoeff*mBallSpd.y;
+            mBallSpd.x += mBallAcl.x;
+            mBallSpd.y += mBallAcl.y;
+            x += mBallSpd.x;
+            y += mBallSpd.y;
+            //if ball goes off screen, reposition to opposite side of screen
+            if (x > mScrWidth){
+                x = mScrWidth;
+                mBallSpd.x = -mBallSpd.x;
+            }
+            if (y > mScrHeight){
+                mBallSpd.y = -mBallSpd.y;
+                y = mScrHeight;
+            }
+            if (x < 0) {
+                mBallSpd.x = -mBallSpd.x;
+                x = 0;
+            }
+            if (y < 0){
+                y = 0;
+                mBallSpd.y = -mBallSpd.y;
+            }
+
+            //redraw ball. Must run in background thread to prevent thread lock.
+            mHandler.post(new Runnable() {
+                public void run() {
+                    view.invalidate();
+                }
+            });
+        }
+    }
+    public void debugWindow (Context context) {
+        View frameLayout = findViewById(R.id.main_view);
+        TextView debugLog = new TextView(context);
+        debugLog.setText("Debug");
+        debugLog.setLayoutParams(new ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT));
+    }
+    public void reset() {
+        if (coinsColected >= 12) {
+            time = System.currentTimeMillis()/1000;
+            coinsColected = 0;
+            placeCoin(1);
+            placeCoin(2);
+            coin1 = true;
+            coin2 = true;
+        }
     }
 }
--- Tilt Ball/src/main/java/com/github/mrthreepwood/tiltball/tiltball/TiltBallActivity.java
+++ Tilt Ball/src/main/java/com/github/mrthreepwood/tiltball/tiltball/TiltBallActivity.java
@@ -22,14 +22,8 @@ import java.util.TimerTask;
 public class TiltBallActivity extends Activity implements SensorEventListener, View.OnTouchListener {
 
     BallView mBallView = null;
-    Handler mHandler = new Handler(); //so redraw occurs in main thread
-    Timer mTmr = null;
-    int mScrWidth, mScrHeight;
-    float dragCoeff = .02f;
     float accelCoeff = .06f;
     PointF mBallPos = new PointF(0,0);
-    PointF mBallSpd = new PointF(0,0);
-    PointF mBallAcl = new PointF(0,0);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,23 +36,12 @@ public class TiltBallActivity extends Activity implements SensorEventListener, V
         //create pointer to main screen
         final FrameLayout mainView =
                 (android.widget.FrameLayout) findViewById(R.id.main_view);
-        //get screen dimensions
-        Display display = getWindowManager().getDefaultDisplay();
-        mScrWidth = display.getWidth();
-        mScrHeight = display.getHeight();
-        mBallPos = new android.graphics.PointF();
-        mBallAcl = new android.graphics.PointF();
-
-        //create variables for ball position and speed
-        mBallPos.x = mScrWidth / 2;
-        mBallPos.y = mScrHeight / 2;
-        mBallAcl.x = 0;
-        mBallAcl.y = 0;
         //create initial ball
-        mBallView = new BallView(this, mBallPos.x, mBallPos.y, 10);
+        mBallView = new BallView(this);
 
         mainView.addView(mBallView); //add ball to main screen
         mBallView.invalidate(); //call onDraw in BallView
+        mBallView.view = mBallView;
         //listener for accelerometer, use anonymous class for simplicity
         ((SensorManager) getSystemService(Context.SENSOR_SERVICE)).registerListener(this,
                 ((SensorManager) getSystemService(Context.SENSOR_SERVICE)).getSensorList(Sensor.TYPE_ACCELEROMETER).get(0),
@@ -72,8 +55,8 @@ public class TiltBallActivity extends Activity implements SensorEventListener, V
     @Override
     public void onPause() //app moved to background, stop background threads
     {
-        mTmr.cancel(); //kill\release timer (our only background thread)
-        mTmr = null;
+        mBallView.mTmr.cancel(); //kill\release timer (our only background thread)
+        mBallView.mTmr = null;
         super.onPause();
     }
 
@@ -82,16 +65,16 @@ public class TiltBallActivity extends Activity implements SensorEventListener, V
     {
         super.onResume();
         //create timer to move ball to new position
-        mTmr = new Timer();
-        mTmr.schedule(new MyTimerTask(), 10, 10); //start timer
+         //start timer
+        mBallView.startTimer();
     }
 
 
     @Override
     public void onSensorChanged(SensorEvent event) {
         //set ball speed based on phone tilt (ignore Z axis)
-        mBallAcl.x = -event.values[0]*accelCoeff;
-        mBallAcl.y = event.values[1]*accelCoeff;
+        mBallView.mBallAcl.x = -event.values[0]*accelCoeff;
+        mBallView.mBallAcl.y = event.values[1]*accelCoeff;
         //timer event will redraw ball
     }
 
@@ -103,55 +86,12 @@ public class TiltBallActivity extends Activity implements SensorEventListener, V
     @Override
     public boolean onTouch(View v, MotionEvent event) {
         //set ball position based on screen touch
-        mBallPos.x = event.getX();
-        mBallPos.y = event.getY();
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+
+        if (x > 10 && x < mBallView.mScrWidth - 10 && y > 10 && y < mBallView.mScrHeight -10)
+            mBallView.reset();
         //timer event will redraw ball
         return true;
-    }
-
-    class MyTimerTask extends TimerTask {
-        public void run() {
-            //if debugging with external device,
-            //  a log cat viewer will be needed on the device
-            //android.util.Log.d("TiltBall", "Timer Hit - " + mBallPos.x + ":" + mBallPos.y);
-            //move ball based on current speed
-            if ((mBallAcl.x < .02f && mBallAcl.x > 0) || (mBallAcl.x > -.02f && mBallAcl.x < 0))
-                mBallAcl.x =0;
-            if ((mBallAcl.y < .02f && mBallAcl.y > 0) || (mBallAcl.y > -.02f && mBallAcl.y < 0))
-                mBallAcl.y =0;
-            mBallAcl.x -= dragCoeff*mBallSpd.x;
-            mBallAcl.y -= dragCoeff*mBallSpd.y;
-            mBallSpd.x += mBallAcl.x;
-            mBallSpd.y += mBallAcl.y;
-            mBallPos.x += mBallSpd.x;
-            mBallPos.y += mBallSpd.y;
-            //if ball goes off screen, reposition to opposite side of screen
-            if (mBallPos.x > mScrWidth){
-                mBallPos.x = mScrWidth;
-                mBallSpd.x = -mBallSpd.x;
-            }
-            if (mBallPos.y > mScrHeight){
-                mBallSpd.y = -mBallSpd.y;
-                mBallPos.y = mScrHeight;
-            }
-            if (mBallPos.x < 0) {
-                mBallSpd.x = -mBallSpd.x;
-                mBallPos.x = 0;
-            }
-            if (mBallPos.y < 0){
-                mBallPos.y = 0;
-                mBallSpd.y = -mBallSpd.y;
-            }
-
-            //update ball class instance
-            mBallView.x = mBallPos.x;
-            mBallView.y = mBallPos.y;
-            //redraw ball. Must run in background thread to prevent thread lock.
-            mHandler.post(new Runnable() {
-                public void run() {
-                    mBallView.invalidate();
-                }
-            });
-        }
     }
 }
